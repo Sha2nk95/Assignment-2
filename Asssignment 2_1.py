30a868421d78814ed8780eb0a6a6b65509c3f4ef
Sha2nk95 <shashankg54@gmail.com>
2025-06-04 13:44:35 +0530

    Add files via upload

--- Asssignment 2_1.py
+++ Asssignment 2_1.py
@@ -1,4 +1,5 @@
 total_sum = 0
-for number in range(1,51):
+for number in range(1, 51):
     total_sum += number
-    print(f"the sum of numbers from 1 to 50 is: {total_sum}")
+print(f"the sum of numbers from 1 to 50 is: {total_sum}")
+print(f"the sum of numbers from 1 to 50 is: {total_sum}")
